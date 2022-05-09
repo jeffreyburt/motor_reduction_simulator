e40bb8a88792011c0661cae984f577ff27793061
Jeffrey Burt <burtj@catlin.edu>
2022-05-08 22:25:29 -0700

    simulator complete, not verified

--- src/Simulator.java
+++ src/Simulator.java
@@ -2,6 +2,92 @@
 //low level class, returns whether a the stick is capable of recovering from a certain given Stick_State
 public class Simulator {
 
+    private final Stick_State state;
+    private final double[][] motor_data;
+    private final double time_delta_sec = 0.05;
+
+
+    public Simulator(Stick_State state, double[][] motor_data){
+        this.state = state;
+        this.motor_data = motor_data;
+    }
+
+    public boolean simulate(){
+        while (true){
+
+            //flywheel calculations and updates
+            double flywheel_torque = calculate_flywheel_torque();
+            update_flywheel_velocity(flywheel_torque);
+
+            //stick calculations and updates
+            double gravity_torque = calculate_gravity_torque();
+            update_stick_angle(flywheel_torque, gravity_torque);
+            update_stick_velocity(flywheel_torque, gravity_torque);
+
+            //testing if stick has failed at recovering from current state
+            if(state.getStick_speed_rad_sec() > 0){
+                return false;
+            }
+
+            //testing if stick has successfully recovered
+            if(state.getStick_deflection_degrees() <= 0){
+                return true;
+            }
+        }
+    }
+
+    //expressed as a negative number
+    private double calculate_flywheel_torque(){
+        double current_motor_speed = state.getMotor_speed_rpm();
+
+        //finding of data points surrounding motor rpm
+        int lower_close_index = 0;
+        for (int i = 0; i < motor_data.length - 1; i++) {
+            if(motor_data[i + 1][0] <= current_motor_speed){
+                lower_close_index = i + 1;
+            }else{ break; }
+        }
+
+        //basic bounds checking
+        if(lower_close_index == motor_data.length) lower_close_index = motor_data.length - 1;
+
+        //linear fit between two surrounding points
+        double slope = (motor_data[lower_close_index][1] - motor_data[lower_close_index + 1][1]) / (motor_data[lower_close_index][0] - motor_data[lower_close_index + 1][0]);
+        double base_torque = ((current_motor_speed - motor_data[lower_close_index][0]) * slope) + motor_data[lower_close_index][1];
+
+        //adjust torque for direction and gear ratio
+        return ((double) -1) * state.getFlywheel_reduction() * base_torque;
+    }
+
+    private void update_flywheel_velocity(double torque){
+        double angular_accel = Math.abs(torque) / state.getFlywheel_moi_kgm2();
+        state.setFlywheel_speed_rad_sec(state.getFlywheel_speed_rad_sec() + (angular_accel * time_delta_sec));
+    }
+
+    //called after new stick angle is calculated
+    private void update_stick_velocity(double flywheel_torque, double gravity_torque){
+        double net_torque = gravity_torque + flywheel_torque;
+
+        double angular_accel = Math.abs(net_torque) / state.getStick_moi_kgm2();
+        state.setStick_speed_rad_sec(state.getStick_speed_rad_sec() + (angular_accel * time_delta_sec));
+    }
+
+    //expressed as a positive number
+    private double calculate_gravity_torque(){
+        return state.getLength_M() * Math.sin(state.getStick_deflection_rad()) * state.getMass_KG() * 9.81;
+    }
+
+    private void update_stick_angle(double flywheel_torque, double gravity_torque){
+        double net_torque = gravity_torque + flywheel_torque;
+        double angular_accel = Math.abs(net_torque) / state.getStick_moi_kgm2();
+
+        double new_deflection = state.getStick_deflection_rad() + (state.getStick_speed_rad_sec() * time_delta_sec) +
+                (0.5 * angular_accel * Math.pow(time_delta_sec, 2));
+        state.setStick_deflection_rad(new_deflection);
+    }
+
+
+
 
 
 
--- src/Stick_State.java
+++ src/Stick_State.java
@@ -1,4 +1,110 @@
 
 //low level class, contains all stick state data
 public class Stick_State {
+
+    //TODO documentation for units
+    private final double flywheel_reduction;
+
+    private final double length_M;
+
+    private final double mass_KG;
+
+    private double stick_speed_rad_sec;
+
+    private double stick_deflection_rad;
+
+    private final double flywheel_moi_kgm2;
+
+    private double flywheel_speed_rad_sec;
+
+
+
+    public Stick_State(double flywheel_reduction, double length_M, double mass_KG, double stick_speed_rad_sec,
+                       double stick_deflection_rad, double flywheel_moi_kgm2, double flywheel_speed_rad_sec){
+
+        this.flywheel_reduction = flywheel_reduction;
+        this.length_M = length_M;
+        this.mass_KG = mass_KG;
+        this.stick_speed_rad_sec = stick_speed_rad_sec;
+        this.stick_deflection_rad = stick_deflection_rad;
+        this.flywheel_moi_kgm2 = flywheel_moi_kgm2;
+        this.flywheel_speed_rad_sec = flywheel_speed_rad_sec;
+    }
+
+    public Stick_State(Stick_State copy_state, double flywheel_reduction, double stick_deflection_rad){
+        flywheel_speed_rad_sec = 0;
+        length_M = copy_state.getLength_M();
+        this.flywheel_reduction = flywheel_reduction;
+        mass_KG = copy_state.getMass_KG();
+        this.stick_deflection_rad = stick_deflection_rad;
+        flywheel_moi_kgm2 = copy_state.flywheel_moi_kgm2;
+        flywheel_speed_rad_sec = 0;
+    }
+
+    public double getFlywheel_reduction(){
+        return flywheel_reduction;
+    }
+
+    public double getLength_M() {
+        return length_M;
+    }
+
+    public double getStick_moi_kgm2(){
+        return mass_KG * Math.pow(length_M,2);
+    }
+
+    public double getFlywheel_moi_kgm2() {
+        return flywheel_moi_kgm2;
+    }
+
+    public double getFlywheel_speed_rad_sec() {
+        return flywheel_speed_rad_sec;
+    }
+
+    //TODO magic number
+    public double getFlywheel_speed_rpm(){
+        return flywheel_speed_rad_sec * 9.5493;
+    }
+
+    public double getMotor_speed_rpm(){
+        return getFlywheel_speed_rpm() * flywheel_reduction;
+    }
+
+    public double getStick_deflection_degrees(){
+        return Math.toDegrees(stick_deflection_rad);
+    }
+
+    public double getStick_deflection_rad() {
+        return stick_deflection_rad;
+    }
+
+    public double getMass_KG() {
+        return mass_KG;
+    }
+
+    public double getStick_speed_rad_sec() {
+        return stick_speed_rad_sec;
+    }
+
+    public void setFlywheel_speed_rad_sec(double flywheel_speed_rad_sec) {
+        this.flywheel_speed_rad_sec = flywheel_speed_rad_sec;
+    }
+
+    public void setFlywheel_speed_rpm(double rpm){
+        flywheel_speed_rad_sec = rpm / 9.5493;
+    }
+
+    public void setStick_deflection_rad(double stick_deflection_rad) {
+        this.stick_deflection_rad = stick_deflection_rad;
+    }
+
+    public void setStick_deflection_degrees(double degrees){
+        stick_deflection_rad = Math.toRadians(degrees);
+    }
+
+    public void setStick_speed_rad_sec(double stick_speed_rad_sec) {
+        this.stick_speed_rad_sec = stick_speed_rad_sec;
+    }
+
+
 }
